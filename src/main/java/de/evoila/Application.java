9355025e8e8bec0d3113c7df0311b14e649acd82
Rene Schollmeyer <rschollmeyer@evoila.de>
2017-06-21 15:56:42 +0200

    chore(dependencies): update dependency version

--- src/main/java/de/evoila/Application.java
+++ src/main/java/de/evoila/Application.java
@@ -8,8 +8,9 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -23,6 +24,8 @@ import de.evoila.cf.cpi.custom.props.MySQLCustomPropertyHandler;
  * @author Johannes Hiemer.
  *
  */
+
+@RefreshScope
 @SpringBootApplication
 @EnableMongoRepositories(basePackages={"de.evoila.cf.cpi.openstack.custom", "de.evoila.cf.broker.persistence.mongodb.repository"})
 public class Application {
